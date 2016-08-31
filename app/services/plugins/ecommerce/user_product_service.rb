481781a0c8aa4b00c14ade5128e9ae37d4d34444
Oleg Pudeyev <oleg@bsdpower.com>
2016-08-31 14:12:25 -0400

    DRY user product service

--- app/services/plugins/ecommerce/user_product_service.rb
+++ app/services/plugins/ecommerce/user_product_service.rb
@@ -9,15 +9,21 @@ class Plugins::Ecommerce::UserProductService
   attr_reader :site, :user, :product, :variation_id
   
   def available_qty
+    available_qty = Plugins::Ecommerce::ProductService.new(
+      site, product, variation_id).available_qty
+    available_qty -= qty_in_carts
+  end
+  
+  private
+  
+  def qty_in_carts
     carts = site.carts.where.not(user_id: user.id).active_cart.joins(:product_items)
     if variation_id.present?
-      (product.decorate.get_variation(variation_id).qty || 0) -
-        carts.where("#{Plugins::Ecommerce::ProductItemDecorator.table_name}" =>
-          {variation_id: variation_id}).sum("#{Plugins::Ecommerce::ProductItem.table_name}.qty")
+      carts.where("#{Plugins::Ecommerce::ProductItemDecorator.table_name}" =>
+        {variation_id: variation_id}).sum("#{Plugins::Ecommerce::ProductItem.table_name}.qty")
     else
-      (product.get_field_value('ecommerce_qty').to_f || 0) -
-        carts.where("#{Plugins::Ecommerce::ProductItemDecorator.table_name}" =>
-          {product_id: product.id}).sum("#{Plugins::Ecommerce::ProductItem.table_name}.qty")
+      carts.where("#{Plugins::Ecommerce::ProductItemDecorator.table_name}" =>
+        {product_id: product.id}).sum("#{Plugins::Ecommerce::ProductItem.table_name}.qty")
     end
   end
 end
