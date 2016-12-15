8486b3e17a32a1711982d7683a9f130654741b56
Owen Peredo <owenperedo@gmail.com>
2016-12-15 05:25:55 -0400

    Added select/deselect all for shipping countries

--- app/assets/javascripts/plugins/ecommerce/admin_settings.js
+++ app/assets/javascripts/plugins/ecommerce/admin_settings.js
@@ -6,4 +6,11 @@ jQuery(function(){
     form.find('#setting_visitor_unit_currencies').multiSelect({keepOrder: true});
     form.find('#setting_shipping_countries').multiSelect();
     setTimeout(function(){ form.find('textarea.editor.translate-item').tinymce(cama_get_tinymce_settings({height: 220})) }, 100);
+    form.find('.shipping_countries_link').click(function(){
+        if($(this).hasClass('select_all'))
+            form.find('#setting_shipping_countries').multiSelect('select_all');
+        else
+            form.find('#setting_shipping_countries').multiSelect('deselect_all');
+        return false;
+    });
 });
